bea414d03a3677245ab48e73db2d68558f135754
Acty101 <junkit.work@gmail.com>
2023-11-19 00:43:28 -0500

    removed Agent based approach, added google place finder class, working models

---
+++ gplace/g_place.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import random
+from typing import List
+
+
+class GPlaceFinder:
+    def __init__(
+        self,
+        url="https://places.googleapis.com/v1/places:searchText",
+        gplaces_api_key=None,
+    ) -> None:
+        self.url = url
+        self.gplaces_api_key = (
+            gplaces_api_key
+            if gplaces_api_key is not None
+            else os.environ.get("GPLACES_API_KEY")
+        )
+
+    def query(self, query: str, num_return = 3) -> List[dict]:
+        """Search for information about places or restaurants"""
+        headers = {
+            "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.location",
+            "X-Goog-Api-Key": self.gplaces_api_key,
+            "Content-Type": "application/json",
+        }
+        payload = {"textQuery": query}
+        response = requests.post(self.url, headers=headers, json=payload)
+        if response.status_code == 200:
+            items = response.json()["places"]
+            items = random.sample(items, num_return)
+            items = [
+                {
+                    "name": item["displayName"]["text"],
+                    "address": item["formattedAddress"],
+                    "location": item["location"],
+                }
+                for item in items
+            ]
+            return items
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return []
---
+++ main.py
@@ -0,0 +1,24 @@
+from gplace import GPlaceFinder
+from LangChain import DuoLangChain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# model, opening_msg = DuoLangChain.construct()
+
+# print(opening_msg)
+
+# while True:
+#     a = input("Enter prompt: ")
+#     result, done = model.invoke(a)
+#     print(result)
+#     if done:
+#         g = GPlaceFinder()
+#         for place in model.places:
+#             print(g.query(place))
+#         print(model.mode)
+#         break
+
+g = GPlaceFinder()
+print(g.query("Japanese food"))
